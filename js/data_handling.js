3cf7fcdd787bb482864921777b23d652c6f71cd7
tbindrup <tbindrup1221@gmail.com>
2015-02-10 17:14:51 -0700

    data_handling.js

--- js/data_handling.js
+++ js/data_handling.js
@@ -3,7 +3,23 @@ var surveys = d3.json('http://localhost:5000/api/survey', function(data){ return
 
 
 function loadSurveys(error, surveys) {
-	console.log(surveys.objects);
+	var surveys_container = d3.select('.surveys');
+	surveys.objects.forEach(function(d) {
+		var new_survey = surveys_container
+						 .append('div')
+						 .attr('class', 'survey col-sm-4')
+						 ;
+		var new_survey_main = new_survey
+							  .append("div")
+							  .attr("class", 'survey-name')
+							  ;
+
+		var new_survey_secondary = new_survey
+								   .append("div")
+								   .attr("class", 'survey-name')
+								   ;
+	})
+	;
 }
 
 
