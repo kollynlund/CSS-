6ab46fa874f46dd9840ef71d24281ed6e6c4f886
kollynlund <kollyn.lund@gmail.com>
2015-02-12 12:56:46 -0700

    disabled scrolling

--- js/survey.js
+++ js/survey.js
@@ -291,3 +291,49 @@ function RageRange () {
 
     });
 }
+
+
+
+
+
+
+
+var disableScroll = false;
+var scrollPos = 0;
+function stopScroll() {
+    disableScroll = true;
+    scrollPos = $(window).scrollTop();
+}
+function enableScroll() {
+    disableScroll = false;
+}
+$(function(){
+    $(window).bind('scroll', function(){
+         if(disableScroll) $(window).scrollTop(scrollPos);
+    });
+    $(window).bind('touchmove', function(){
+         $(window).trigger('scroll');
+    });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
